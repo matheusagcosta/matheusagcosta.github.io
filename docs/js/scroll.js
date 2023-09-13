c1fee61c315e3373857f9b3ea28bd282a9933898
matheusagcosta <matheusagcosta@gmail.com>
2023-09-13 15:32:58 -0300

    Add scroll to and header links

---
+++ docs/js/scroll.js
@@ -0,0 +1,23 @@
+// scroll into work view
+document.querySelector('.js-scroll-to-work').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.work').scrollIntoView({ behavior: 'smooth' });
+});
+
+// scroll into work view 2
+document.querySelector('.js-scroll-to-work-alt').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.work').scrollIntoView({ behavior: 'smooth' });
+});
+
+// scroll into about view
+document.querySelector('.js-scroll-to-about').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.about').scrollIntoView({ behavior: 'smooth' });
+});
+
+// scroll into talks view
+document.querySelector('.js-scroll-to-talks').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector('.talks').scrollIntoView({ behavior: 'smooth' });
+});
